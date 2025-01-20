1e377292515ea9724cd59051b15b62b390be4936
Pancake <baligajozsef@gmail.com>
2025-01-20 08:15:57 +0100

    -

---
+++ javagyak/gyak5.js
@@ -0,0 +1,7 @@
+let keresztnev = prompt("Mi a keresztneved: ")
+let vezeteknev = prompt("Mi a vezetékneved: ")
+let osztaly = prompt("Milyen osztályba jársz")
+let kedvenc = prompt("Mi a kedvenc alkoholos itókád")
+let nev = keresztnev + vezeteknev
+console.log("Név: ", nev, "Osztály: ", osztaly, "Kedvenc szesz: ", kedvenc)
+document.writeln("Név: ", nev, "Osztály: ", osztaly, "Kedvenc szesz: ", kedvenc)
