2e6f33748b9c645d1fa126e01b65f50793d864a9
Pancake <baligajozsef@gmail.com>
2025-06-04 14:16:02 +0200

    -

--- dog/dog.js
+++ dog/dog.js
@@ -1,9 +1,46 @@
-function getPic (){
-    fetch('https://dog.ceo/api/breeds/image/random')
+function getPic1(){
+    fetch('https://dog.ceo/api/breed/akita/images/random')
     .then(response=>{
         return response.json();
     })
     .then(data=>{
-        document.getElementById("pic").src = data.message
+        document.getElementById("pic").src = data.message;
     })
 }
+function getPic2(){
+    fetch('https://dog.ceo/api/breed/african/images/random')
+    .then(response=>{
+        return response.json();
+    })
+    .then(data=>{
+        document.getElementById("pic").src = data.message;
+    })
+}
+function getPic3(){
+    fetch('https://dog.ceo/api/breed/beagle/images/random')
+    .then(response=>{
+        return response.json();
+    })
+    .then(data=>{
+        document.getElementById("pic").src = data.message;
+    })
+}
+function getPic4(){
+    fetch('https://dog.ceo/api/breed/boxer/images/random')
+    .then(response=>{
+        return response.json();
+    })
+    .then(data=>{
+        document.getElementById("pic").src = data.message;
+    })
+}
+
+const gombok = document.querySelectorAll('#gombok button');
+gombok.forEach(gomb => {
+    gomb.onmouseover = function() {
+        gomb.style.backgroundColor = "#a5a5a5";
+    };
+    gomb.onmouseout = function() {
+        gomb.style.backgroundColor = "#cccccc";
+    };
+});
