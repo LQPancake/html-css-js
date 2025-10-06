10f2057c5825db04f8336003b0890e90253dd86c
Pancake <baligajozsef@gmail.com>
2025-10-06 09:31:08 +0200

    -

--- rubikMas/rubik.js
+++ rubikMas/rubik.js
@@ -6,10 +6,12 @@ const cimkep = document.getElementById('cimkep');
   cimkep.onmouseout = function (){
     cimkep.src = "sematikus.png";}
 // 2.    
-var erdekessegek = document.getElementsByClassName('erdekesseg');
-    erdekessegek.onclick = function(){
-    erdekessegek.classList.add("nagyobb")
+const erdekessegek = document.querySelectorAll('.erdekesseg');
+for (const erdekesseg of erdekessegek){
+    erdekesseg.onclick = function(event){
+      erdekesseg.classList.toggle('nagyobb');
     }
+}
 // 3.
 const szul_ev=document.getElementById('szul_ev').value;
 szul_ev.onsubmit=function(event){
