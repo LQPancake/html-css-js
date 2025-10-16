8c495111808ed748b1fbe758466a868006746011
Pancake <baligajozsef@gmail.com>
2025-10-16 08:45:03 +0200

    -

---
+++ webshop-project/script.js
@@ -0,0 +1,39 @@
+let state = {
+    products : [
+        {
+            name: "Teszt termék 1",
+            price: 2500,
+            isInStock: true
+        },
+        {
+            name: "Teszt termék 2",
+            price: 3500,
+            isInStock: true
+        },
+        {
+            name: "Teszt termék 3",
+            price: 5550,
+            isInStock: false
+        }
+    ]
+}
+function renderProducts() {
+    var productsHTML = '';
+    for (const products of state.products){
+        productsHTML +=`
+        <div class="card m-2 p-2 ${products.isInStock ? "" : "bg-danger"}">
+            <p>${products.name}</p>
+            <p>${products.price}</p>
+        </div>
+        `
+    }
+    document.getElementById('product-list-component').innerHTML = productsHTML;
+}
+window.onload = renderProducts;
+document.getElementById('create-product').onsubmit =  (event) => {
+    event.preventDefault();
+    console.log(event.target.elements.price.name.value);
+    console.log(event.target.elements.price.name.value);
+    console.log(event.target.elements.price.isInStock);
+
+}
