743df21ac33e36e94f00f49ecf87fdd2ea3bc4b7
Pancake <baligajozsef@gmail.com>
2025-11-25 08:43:23 +0100

    -

--- maszekkiralysag/script.js
+++ maszekkiralysag/script.js
@@ -1,48 +1,55 @@
 state = {
   workers: [
     {
+      id: crypto.randomUUID,
       name: "Miss Madelen Årdal",
       city: "Eggkleiva",
       email: "madelen.ardal@example.com",
       picture: "https://randomuser.me/api/portraits/women/88.jpg",
     },
     {
+      id: crypto.randomUUID,
       name: "Miss Nila Rugland",
       city: "Sommarøy",
       email: "nila.rugland@example.com",
       picture: "https://randomuser.me/api/portraits/women/68.jpg",
     },
     {
+      id: crypto.randomUUID,
       name: "Mr Diego Aanestad",
       city: "Fuglevik",
       email: "diego.aanestad@example.com",
       picture: "https://randomuser.me/api/portraits/men/83.jpg",
     },
     {
+      id: crypto.randomUUID,
       name: "Mr Nikolai Lidal",
       city: "Liland",
       email: "nikolai.lidal@example.com",
       picture: "https://randomuser.me/api/portraits/men/82.jpg",
     },
     {
+      id: crypto.randomUUID,
       name: "Ms Goda Skinnes",
       city: "Bjørkelangen",
       email: "goda.skinnes@example.com",
       picture: "https://randomuser.me/api/portraits/women/62.jpg",
     },
     {
+      id: crypto.randomUUID,
       name: "Ms Mari Abdirahman",
       city: "Øyenkilen",
       email: "mari.abdirahman@example.com",
       picture: "https://randomuser.me/api/portraits/women/85.jpg",
     },
     {
+      id: crypto.randomUUID,
       name: "Mr Stanislaw Sørheim",
       city: "Porsgrunn",
       email: "stanislaw.sorheim@example.com",
       picture: "https://randomuser.me/api/portraits/men/87.jpg",
     },
-    {
+    {id: crypto.randomUUID,
       name: "Ms Thelma Hope",
       city: "Skre",
       email: "thelma.hope@example.com",
@@ -50,7 +57,7 @@ state = {
     },
   ],
 };
-function render() {
+function renderWorkers() {
     let workers = document.getElementById("workers")
     workerHtml = '<div class="row">';
     for (const worker of state.workers) {
@@ -60,11 +67,30 @@ function render() {
                     <img src="${worker.picture}" alt="">
                     <h3> ${worker.city}</h3>
                     <p>${worker.name}</p>
-                    <p id="email">${worker.email}</p>    
+                    <p id="email">${worker.email}</p> 
+                    <button class="btn btn-danger float-right delete-worker" data-workerid="${worker.id}">Törlés</button>   
                 </div>
             </div>`
             }
             workerHtml+= "</div>";
             workers.innerHTML = workerHtml;
         }
-window.onload = render;
+
+window.onload = renderWorkers();
+document.getElementById('create-worker').onsubmit =  (event) => {
+    event.preventDefault();
+    const id = crypto.randomUUID();
+    const picture = event.target.elements.picture.value;
+    const city = event.target.elements.city.value;
+    const name = event.target.elements.name.value;
+    const email = event.target.elements.email.value;
+    state.workers.push({
+        id,
+        picture,
+        city,
+        name,
+        email,
+    });
+    renderWorkers();
+    // még kell torles
+}
--- webshop-project/script.js
+++ webshop-project/script.js
@@ -1,16 +1,19 @@
 let state = {
     products : [
         {
+            id: crypto.randomUUID(),
             name: "Teszt termék 1",
             price: 2500,
             isInStock: true
         },
         {
+            id: crypto.randomUUID(),
             name: "Teszt termék 2",
             price: 3500,
             isInStock: true
         },
         {
+            id: crypto.randomUUID(),
             name: "Teszt termék 3",
             price: 5550,
             isInStock: false
@@ -19,23 +22,40 @@ let state = {
 }
 function renderProducts() {
     var productsHTML = '';
-    for (const products of state.products){
+    for (const product of state.products){
         productsHTML +=`
-        <div class="card m-2 p-2 ${products.isInStock ? "" : "bg-danger"}">
-            <p>${products.name}</p>
-            <p>${products.price}</p>
+        <div class="card m-2 p-2 ${product.isInStock ? "" : "bg-danger"}">
+            <p>${product.name}</p>
+            <p>${product.price}</p>
+            <button class="btn btn-danger float-right delete-product" data-productid="${product.id}">Törlés</button>
         </div>
         `
     }
     document.getElementById('product-list-component').innerHTML = productsHTML;
+    for (const deleteBtn of document.querySelectorAll(".delete-product")){
+        deleteBtn.onclick = function(event){
+        let id = event.target.dataset.productid;
+        let foundIndex;
+        for (let index = 0; index < state.products.length; index++){
+            if (state.products[index].id == id){
+                foundIndex = index;
+                break;
+            }
+            }
+        state.products.splice(foundIndex, 1)
+        renderProducts();
+        }
+    }
 }
 window.onload = renderProducts;
 document.getElementById('create-product').onsubmit =  (event) => {
     event.preventDefault();
+    const id = crypto.randomUUID();
     const name = event.target.elements.name.value;
     const price = event.target.elements.price.value;
     const isInStock = event.target.elements.isInStock.checked;
     state.products.push({
+        id,
         name,
         price,
         isInStock
