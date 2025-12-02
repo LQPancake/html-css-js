f896b9eae6adf4e9ffeb61aa4744a1eb43c57d8c
Pancake <baligajozsef@gmail.com>
2025-12-02 08:44:24 +0100

    -

--- webshop-project/script.js
+++ webshop-project/script.js
@@ -20,17 +20,70 @@ let state = {
         }
     ],
     editedId: ""
-}
-    function renderEditProduct(){
-        let foundProduct;
-        for(const product of state.products){
-            if(product id === state.editedId){
-                foundProduct = product;
+};
+
+function renderEditProduct(){    
+    if(state.editedId==""){
+        document.getElementById("edit-product").innerHTML="";
+        return
+    }
+    let foundProduct;
+    for (const product of state.products) {
+        if (product.id === state.editedId){
+            foundProduct = product;
+            break;
+        }
+    }
+
+    let editFormHTML = `
+        <h3>Termék szerkesztése</h3>
+        <form id="update-product" class="p-5">
+            <label class="w-100">
+              Név:
+              <input class="form-control" type="text" name="name" value="${foundProduct.name}">
+            </label>
+            <label class="w-100">
+              Ár:
+              <input class="form-control" type="number" name="price" value="${foundProduct.price}">
+            </label>
+            <label class="w-100">
+              Van készleten?
+              <input class="form-control" type="checkbox" name="isInStock" ${foundProduct.isInStock ? "checked" : ""}>
+            </label>
+            <button class="btn btn-primary" type="submit">Küldés</button>
+        </form>
+    `;
+
+    document.getElementById("edit-product").innerHTML = editFormHTML;
+
+    document.getElementById('update-product').onsubmit = (event) => {
+        event.preventDefault();
+
+        const name = event.target.elements.name.value;
+        const price = event.target.elements.price.value;
+        const isInStock = event.target.elements.isInStock.checked;
+
+        let foundIndex;
+
+        for (let index = 0; index < state.products.length; index++){
+            if (state.products[index].id == state.editedId){
+                foundIndex = index;
                 break;
             }
         }
-    }
+
+        state.products[foundIndex] = {
+            id: state.editedId,
+            name,
+            price,
+            isInStock
+        }
+        state.editedId=""
+        renderProducts();
+        renderEditProduct();
+    };
 }
+
 function renderProducts() {
     var productsHTML = '';
     for (const product of state.products){
@@ -38,40 +91,48 @@ function renderProducts() {
         <div class="card m-2 p-2 ${product.isInStock ? "" : "bg-danger"}">
             <p>${product.name}</p>
             <p>${product.price}</p>
+            <button class="btn btn-warning float-right edit-product md-2" data-productid="${product.id}">Szerkesztés</button>
             <button class="btn btn-danger float-right delete-product" data-productid="${product.id}">Törlés</button>
-            <button class="btn btn-warning float-right edit-product" data-productid="${product.id}">Szerkesztés</button>
         </div>
-        `
+        `;
     }
     document.getElementById('product-list-component').innerHTML = productsHTML;
+
     for(const editBtn of document.querySelectorAll(".edit-product")){
-        editBtn.onclick = function(event){
+        editBtn.onclick = (event) => { 
             let id = event.target.dataset.productid;
             state.editedId = id;
+            renderEditProduct();
         }
     }
-    for (const deleteBtn of document.querySelectorAll(".delete-product")){
-        deleteBtn.onclick = function(event){
-        let id = event.target.dataset.productid;
-        let foundIndex;
-        for (let index = 0; index < state.products.length; index++){
-            if (state.products[index].id == id){
-                foundIndex = index;
-                break;
+
+    for(const deleteBtn of document.querySelectorAll(".delete-product")){
+        deleteBtn.onclick = (event) => {
+            let id = event.target.dataset.productid;
+            let foundIndex;
+
+            for(let index=0; index < state.products.length; index++){
+                if(state.products[index].id == id){
+                    foundIndex = index;
+                    break;
+                }
             }
-            }
-        state.products.splice(foundIndex, 1)
-        renderProducts();
+
+            state.products.splice(foundIndex, 1);
+            renderProducts();
         }
     }
 }
+
 window.onload = renderProducts;
-document.getElementById('create-product').onsubmit =  (event) => {
+
+document.getElementById('create-product').onsubmit = (event) => {
     event.preventDefault();
     const id = crypto.randomUUID();
     const name = event.target.elements.name.value;
     const price = event.target.elements.price.value;
     const isInStock = event.target.elements.isInStock.checked;
+
     state.products.push({
         id,
         name,
@@ -80,4 +141,4 @@ document.getElementById('create-product').onsubmit =  (event) => {
     });
 
     renderProducts();
-}
+};
