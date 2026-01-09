ba4a76d247b190d05ae951e503f143217426b80f
Pancake <baligajozsef@gmail.com>
2026-01-09 08:44:53 +0100

    -

--- akcio/script.js
+++ akcio/script.js
@@ -20,16 +20,35 @@ order.onchange = () => {
     switch(order.value){
         case "0":
             productsArray.sort((a, b) => {
-                let aValue = a.querySelector(".producct-price").innerText.replace(".", "")
-                let bValue = b.querySelector(".producct-price").innerText.replace(".", "")
-                return a-b
+                let aValue = Number(a.querySelector(".product-price").innerText.replace(".", "").replace(" Ft", ""))
+                let bValue = Number(b.querySelector(".product-price").innerText.replace(".", "").replace(" Ft", ""))
+                return aValue-bValue;
             })
             break;
         case "1":
+        productsArray.sort((a, b) => {
+                let aValue = Number(a.querySelector(".product-price").innerText.replace(".", "").replace(" Ft", ""))
+                let bValue = Number(b.querySelector(".product-price").innerText.replace(".", "").replace(" Ft", ""))
+                return bValue-aValue;
+        })
             break;
         case "2":
+                productsArray.sort((a, b) => {
+                let aValue = a.querySelector(".product-name").innerText;
+                let bValue = b.querySelector(".product-name").innerText;
+                return aValue.localeCompare(bValue)
+        })
             break;
         case "3":
+                productsArray.sort((a, b) => {
+                let aValue = a.querySelector(".product-name").innerText;
+                let bValue = b.querySelector(".product-name").innerText;
+                return bValue.localeCompare(aValue);
+            })
             break;
+    }
+    document.querySelector(".products").innerHTML = ""
+    for (const product of productsArray){
+        document.querySelector(".products").appendChild(product);
     }
 }
