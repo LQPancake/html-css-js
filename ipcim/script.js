fe22c05e31fb9ce73980771b341ec6aebbce76a0
Pancake <baligajozsef@gmail.com>
2026-02-12 08:44:08 +0100

    -

--- ipcim/script.js
+++ ipcim/script.js
@@ -1,11 +1,12 @@
 let ipcim = ""
+
 fetch("https://api.ipify.org/?format=json")
 .then(res => res.json())
 .then(data => {
     ipcim = data.ip;
     fetch(`https://ipinfo.io/${ipcim}/geo`)
-    .then(response = response.json)
+    .then(response => response.json())
     .then(data => {
-        document.getElementsById("data").innerHTML = `<p>${data.hostname}</p>`
+        document.getElementById("data").innerHTML = `<p>${data.hostname}</p>`
     })
 })
