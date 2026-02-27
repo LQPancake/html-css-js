74ce853344e0e61a26d4a0985aaf436e755652e1
Pancake <baligajozsef@gmail.com>
2026-02-27 08:37:14 +0100

    -

--- csudijo/web/csudijo.js
+++ csudijo/web/csudijo.js
@@ -3,3 +3,19 @@ fetch("/api/legnepszerubb")
 .then(data =>{
     document.getElementById("legnepszerubb").innerText = data.etelNev;
 })
+function vendegkonyv() {
+    const bejegyzes = document.getElementById("bejegyzes").value;
+    fetch("/api/vendegkonyv",{
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({bejegyzes: bejegyzes})
+    })
+    .then(response =>{
+        if (response.ok){
+            document.getElementById("bejegyzes").value = "";
+            alert("Köszönjük a bejegyzését!");
+        }
+    })
+}
